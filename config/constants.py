f248f5b0e423e35a47ebe36dcffcaf50e9b7a5c4
Pepe Valdivia <jvaldivia@softweb.pe>
2018-06-20 19:44:41 -0500

    Login contra usuario y contraseña de constants

--- config/constants.py
+++ config/constants.py
@@ -5,5 +5,9 @@ constants = {
     'secret' : "PKBcauXg6sTXz7Ddlty0nejVgoUodXL89KNxcrfwkEme0Huqtj6jjt4fP7v2uF4L",
     'key' : "csrf_val"
   },
+  'login' : {
+    'usuario' : 'admin',
+    'contrasenia' : 'sistema123'
+  },
   'ambiente_static' : 'desarrollo',
 }
--- views/login.py
+++ views/login.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import json
-from bottle import Bottle, request, template
+from bottle import Bottle, request, template, HTTPResponse, redirect
 from config.middleware import enable_cors, headers
 from config.database import engine, session_db
 from config.constants import constants
@@ -25,3 +25,44 @@ def index():
     'mensaje': '',
   }
   return template('templates/login/index', locals = locals, helpers = helpers)
+
+@login_view.route('/acceder', method='POST')
+@headers
+def acceder():
+  mensaje = ''
+  continuar = True
+  csrf_request = request.forms.get(constants['CSRF']['key'])
+  if csrf_request == None:
+    mensaje = 'Token CSRF no existe en POST request'
+    continuar = False
+  else:
+    # validar csrf token
+    if csrf_request != constants['CSRF']['secret']:
+      mensaje = 'Token CSRF no existe en POST request'
+      continuar = False
+    # validar usuario y contraseña si csrf token es correcto
+    if continuar == True:
+      usuario_request = request.forms.get('usuario')
+      contrasenia_request = request.forms.get('contrasenia')
+      print(usuario_request)
+      print(contrasenia_request)
+      if (usuario_request != constants['login']['usuario']) or (contrasenia_request != constants['login']['contrasenia']):
+        mensaje = 'Usuario y/o contraenia no coinciden'
+        continuar = False
+  if continuar == True:
+    #TODO activar la session
+    return redirect("/accesos/")
+  else:
+    helpers = {}
+    helpers['css'] = load_css(login_index_css())
+    helpers['js'] = load_js(login_index_js())
+    locals = {
+      'constants': constants,
+      'title': 'Bienvenido',
+      'data': json.dumps({
+        'modulo' : 'Accesos',
+      }),
+      'mensaje': mensaje,
+    }
+    boby_template = template('templates/login/index', locals = locals, helpers = helpers)
+    return HTTPResponse(status = 500, body = boby_template)
