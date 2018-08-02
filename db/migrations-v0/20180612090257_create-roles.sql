4c090ffc128948f02ea6ea53749498a1dcbe68b7
Pepe Valdivia <jvaldivia@softweb.pe>
2018-08-01 21:02:01 -0500

    Migración, fill roles

--- db/migrations-v0/20180612090257_create-roles.sql
+++
@@ -1,9 +0,0 @@
--- migrate:down
-DROP TABLE IF EXISTS roles;
--- migrate:up
-CREATE TABLE roles(
-	id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	nombre	VARCHAR(30) NOT NULL,
-  sistema_id INTEGER,
-  FOREIGN KEY (sistema_id) REFERENCES sistemas(id) ON DELETE CASCADE
-)
--- db/migrations/20180802020131_fill_roles.sql
+++ db/migrations/20180802020131_fill_roles.sql
@@ -1,7 +1,8 @@
--- migrate:down
---TRUNCATE TABLE roles;
 -- migrate:up
+
 INSERT INTO roles (nombre, sistema_id) VALUES (
   'Administrador',
   1
 );
+
+-- migrate:down
