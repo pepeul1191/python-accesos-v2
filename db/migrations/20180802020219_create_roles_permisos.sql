302ec19a0f0b7747938057d42a67bdad8d9c8d99
Pepe Valdivia <jvaldivia@softweb.pe>
2018-08-01 21:04:11 -0500

    Migración, create roles_permisos

--- db/migrations/20180802020219_create_roles_permisos.sql
+++ db/migrations/20180802020219_create_roles_permisos.sql
@@ -1,6 +1,5 @@
--- migrate:down
-DROP TABLE IF EXISTS roles_permisos;
 -- migrate:up
+
 CREATE TABLE roles_permisos(
 	id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   rol_id INTEGER,
@@ -8,3 +7,7 @@ CREATE TABLE roles_permisos(
   FOREIGN KEY (rol_id) REFERENCES roles(id) ON DELETE CASCADE,
   FOREIGN KEY (permiso_id) REFERENCES permisos(id) ON DELETE CASCADE
 )
+
+-- migrate:down
+
+DROP TABLE IF EXISTS roles_permisos;
