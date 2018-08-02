0a3206510b3f765b98e33a8490f3afa520c93981
Pepe Valdivia <jvaldivia@softweb.pe>
2018-08-01 19:44:25 -0500

    Migración, create items

--- db/migrations/20180802004137_create_items.sql
+++ db/migrations/20180802004137_create_items.sql
@@ -1,6 +1,5 @@
--- migrate:down
-DROP TABLE IF EXISTS items;
 -- migrate:up
+
 CREATE TABLE items(
 	id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	nombre	VARCHAR(30) NOT NULL,
@@ -8,3 +7,7 @@ CREATE TABLE items(
   subtitulo_id INTEGER,
   FOREIGN KEY (subtitulo_id) REFERENCES subtitulos(id) ON DELETE CASCADE
 )
+
+-- migrate:down
+
+DROP TABLE IF EXISTS items;
