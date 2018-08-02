dc6f44553686d2cd2b636c8394a6dc4f74c14d4d
Pepe Valdivia <jvaldivia@softweb.pe>
2018-08-01 21:00:07 -0500

    Migración, create permisos

--- db/migrations/20180802015932_create_permisos.sql
+++ db/migrations/20180802015932_create_permisos.sql
@@ -1,6 +1,5 @@
--- migrate:down
-DROP TABLE IF EXISTS permisos;
 -- migrate:up
+
 CREATE TABLE permisos(
 	id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	nombre	VARCHAR(30) NOT NULL,
@@ -8,3 +7,7 @@ CREATE TABLE permisos(
   sistema_id INTEGER,
   FOREIGN KEY (sistema_id) REFERENCES sistemas(id) ON DELETE CASCADE
 )
+
+-- migrate:down
+
+DROP TABLE IF EXISTS permisos;
