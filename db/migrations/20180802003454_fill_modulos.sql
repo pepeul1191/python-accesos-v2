101f3d654f1796f663bb38cfd8f34f580fb88a6e
Pepe Valdivia <jvaldivia@softweb.pe>
2018-08-01 19:35:41 -0500

    Migración create modulos

--- db/migrations/20180802003454_fill_modulos.sql
+++ db/migrations/20180802003454_fill_modulos.sql
@@ -1,9 +1,10 @@
--- migrate:down
---TRUNCATE TABLE modulos;
---UP
+-- migrate:up
+
 INSERT INTO modulos (nombre, url, icono, sistema_id) VALUES (
   'Accesos',
   'accesos/',
   '',
   1
 );
+
+-- migrate:down
