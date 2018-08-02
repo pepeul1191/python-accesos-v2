40bf272849e6ce9b73cc4e1f091b7d4b12eacd46
Pepe Valdivia <jvaldivia@softweb.pe>
2018-08-01 21:22:33 -0500

    Migración, create usuarios_roles

--- db/migrations/20180802022155_create_usuarios_roles.sql
+++ db/migrations/20180802022155_create_usuarios_roles.sql
@@ -1,6 +1,5 @@
--- migrate:down
-DROP TABLE IF EXISTS usuarios_roles;
 -- migrate:up
+
 CREATE TABLE usuarios_roles(
 	id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   usuario_id INTEGER,
@@ -8,3 +7,7 @@ CREATE TABLE usuarios_roles(
   FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE,
   FOREIGN KEY (rol_id) REFERENCES roles(id) ON DELETE CASCADE
 )
+
+-- migrate:down
+
+DROP TABLE IF EXISTS usuarios_roles;
